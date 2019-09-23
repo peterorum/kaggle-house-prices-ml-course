15157ded307a5f1f2235503f4544428c4a70b57c
Peter Orum <peter@peterorum.com>
2019-09-23 11:40:20 +1000

    lesson 2

--- src/002-lesson.py
+++ src/002-lesson.py
@@ -1,9 +1,10 @@
 # simple impute
-# local score 23528
-# kaggle score 21768 - SHOULD BE 16640.85750
+# local score 18125
+# kaggle score 16640
 # minimize score
 
 import pandas as pd
+import sys  # noqa
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
@@ -13,15 +14,24 @@ from sklearn.impute import SimpleImputer
 X_full = pd.read_csv('../input/train.csv', index_col='Id')
 X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
 
-# Obtain target and predictors
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
 y = X_full.SalePrice
-features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X = X_full[features].copy()
-X_test = X_test_full[features].copy()
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, we'll use only numerical predictors
+X = X_full.select_dtypes(exclude=['object'])
+X_test = X_test_full.select_dtypes(exclude=['object'])
 
 # Break off validation set from training data
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
                                                       random_state=0)
+
+cols_with_missing = [col for col in X_train.columns
+                     if X_train[col].isnull().any()]
+
+reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
 
 my_imputer = SimpleImputer()
 
@@ -33,20 +43,20 @@ imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
 imputed_X_train.columns = X_train.columns
 imputed_X_valid.columns = X_valid.columns
 
-my_model = RandomForestRegressor(n_estimators=100, criterion='mae', random_state=0)
+final_X_train = imputed_X_train
+final_X_valid = imputed_X_valid
+
+my_model = RandomForestRegressor(n_estimators=100, random_state=0)
 
 # Fit the model to the training data & validate for score
-my_model.fit(imputed_X_train, y_train)
-preds_valid = my_model.predict(imputed_X_valid)
+my_model.fit(final_X_train, y_train)
+preds_valid = my_model.predict(final_X_valid)
 score = mean_absolute_error(y_valid, preds_valid)
 print(f'score: {score}')
 
-# Generate test predictions on full set
-my_model.fit(imputed_X_train, y_train)
+final_X_test = pd.DataFrame(my_imputer.fit_transform(X_test))
 
-imputed_X_test = pd.DataFrame(my_imputer.fit_transform(X_test))
-
-preds_test = my_model.predict(imputed_X_test)
+preds_test = my_model.predict(final_X_test)
 
 # Save predictions in format used for competition scoring
 output = pd.DataFrame({'Id': X_test.index,
